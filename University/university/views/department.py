01bccd8dea62980a4c309d09a6a5ad2ad75ff10a
yukselcodes <e2264638@ceng.metu.edu.tr>
2019-07-21 18:48:36 +0300

    views converted to django generic views

--- University/university/views/department.py
+++ University/university/views/department.py
@@ -1,23 +1,77 @@
-from rest_framework import generics
-
-from ..models import Department
-from ..serializers import DepartmentSerializer
-
-
-class List(generics.ListAPIView):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from ..models import Department, Course, Professor
+from ..helper import Constants
 
 
-class Create(generics.CreateAPIView):
-    serializer_class = DepartmentSerializer
+class List(ListView):
+    constants = Constants.get_constants("department", "list", "departments", Department, 10)
+    model = constants.get("MODEL_NAME", None)
+    context_object_name = constants.get("CONTEXT_OBJECT_NAME", None)
+    template_name = constants.get("TEMPLATE_NAME", None)
+    object_list = []
+    paginate_by = constants.get("PAGINATE_BY", 10)
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context)
+
+    def get_context_data(self, **kwargs):
+        context = super(List, self).get_context_data(**kwargs)
+        context['departments'] = self.get_queryset()
+        return context
+
+    def get_queryset(self):
+        departments = []
+        all_departments = Department.objects.all()
+        for d in all_departments:
+            professors = Professor.objects.all().filter(department=d.id)
+            courses = Course.objects.all().filter(department=d.id)
+            departments.append({'department': d, 'courses': courses, "professors": professors})
+        return departments
 
 
-class Update(generics.RetrieveUpdateAPIView):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
+class Detail(DetailView):
+    constants = Constants.get_constants("department", "detail", "department", Department, 10)
+    model = constants.get("MODEL_NAME", None)
+    context_object_name = constants.get("CONTEXT_OBJECT_NAME", None)
+    template_name = constants.get("TEMPLATE_NAME", None)
+    object = {}
+    paginate_by = constants.get("PAGINATE_BY", 10)
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context)
+
+    def get_context_data(self, **kwargs):
+        context = super(Detail, self).get_context_data(**kwargs)
+        d = Department.objects.get(pk=self.kwargs.get('pk'))
+        courses = Course.objects.all().filter(department=d.id)
+        professors = Professor.objects.all().filter(department=d.id)
+        context['department'] = {'department': d, "courses": courses, "professors": professors}
+        return context
 
 
-class Detail(generics.RetrieveAPIView):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
+class Create(CreateView):
+    constants = Constants.get_constants(template_file="department", action="create", model=Department)
+    model = constants.get('MODEL_NAME', None)
+    template_name = constants.get('TEMPLATE_NAME')
+    fields = ("id", "name", "code")
+
+    def get_success_url(self):
+        return reverse_lazy('university:department_list')
+
+
+class Update(UpdateView):
+    constants = Constants.get_constants(template_file="department", action="update", model=Department)
+    model = constants.get('MODEL_NAME', None)
+    template_name = constants.get('TEMPLATE_NAME')
+    fields = ("name", "code")
+
+    def get_success_url(self):
+        return reverse_lazy('university:department_detail', kwargs={'pk': self.kwargs['pk']})
+
+
+
+
--- University/university/views/student.py
+++ University/university/views/student.py
@@ -1,23 +1,74 @@
-from rest_framework import generics
-
-from ..models import Student
-from ..serializers import StudentSerializer
-
-
-class List(generics.ListAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from ..models import Department, Course, Professor, Student
+from ..helper import Constants
 
 
-class Create(generics.CreateAPIView):
-    serializer_class = StudentSerializer
+class List(ListView):
+    constants = Constants.get_constants("student", "list", "students", Student, 10)
+    model = constants.get("MODEL_NAME", None)
+    context_object_name = constants.get("CONTEXT_OBJECT_NAME", None)
+    template_name = constants.get("TEMPLATE_NAME", None)
+    object_list = []
+    paginate_by = constants.get("PAGINATE_BY", 10)
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        print(context)
+        return render(request, self.template_name, context)
+
+    def get_context_data(self, **kwargs):
+        context = super(List, self).get_context_data(**kwargs)
+        context['students'] = self.get_queryset()
+        return context
+
+    def get_queryset(self):
+        students = []
+        all_students = Student.objects.all()
+        for s in all_students:
+            department = Department.objects.all().filter(id=s.department.id)
+            students.append({'student': s, 'department': department})
+        return students
 
 
-class Update(generics.RetrieveUpdateAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+class Detail(DetailView):
+    constants = Constants.get_constants("student", "detail", "student", Student, 10)
+    model = constants.get("MODEL_NAME", None)
+    context_object_name = constants.get("CONTEXT_OBJECT_NAME", None)
+    template_name = constants.get("TEMPLATE_NAME", None)
+    object = {}
+    paginate_by = constants.get("PAGINATE_BY", 10)
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context)
+
+    def get_context_data(self, **kwargs):
+        context = super(Detail, self).get_context_data(**kwargs)
+        s = Student.objects.get(pk=self.kwargs.get('pk'))
+        department = Department.objects.all().filter(id=s.department.id)
+        context['student'] = {'department': department, "student": s}
+        return context
 
 
-class Detail(generics.RetrieveAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+class Create(CreateView):
+    constants = Constants.get_constants(template_file="student", action="create", model=Student)
+    model = constants.get('MODEL_NAME', None)
+    template_name = constants.get('TEMPLATE_NAME')
+    fields = ("id", "name", "surname", "gpa", "course", "department")
+
+    def get_success_url(self):
+        return reverse_lazy('university:student_list')
+
+
+class Update(UpdateView):
+    constants = Constants.get_constants(template_file="student", action="update", model=Student)
+    model = constants.get('MODEL_NAME', None)
+    template_name = constants.get('TEMPLATE_NAME')
+    fields = ("name", "surname", "gpa", "course", "department")
+
+    def get_success_url(self):
+        return reverse_lazy('university:student_detail', kwargs={'pk': self.kwargs['pk']})
+
+
