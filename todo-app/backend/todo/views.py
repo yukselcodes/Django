a2643127d36d1fa42ab66eca2b11ab1a768fed4d
yukselcodes <e2264638@ceng.metu.edu.tr>
2019-08-11 21:46:13 +0300

    add CRUD view for todo model

--- todo-app/backend/todo/views.py
+++ todo-app/backend/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import TodoSerializer
+from .models import Todo
+
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
