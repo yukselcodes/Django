06f789da02072d61e02cd13254d0603f21bcd7fc
yukselcodes <e2264638@ceng.metu.edu.tr>
2019-08-18 15:09:41 +0300

    deleted tutorial

--- rest_framework/backend/rest_framework_tutorial/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestFrameworkTutorialConfig(AppConfig):
-    name = 'rest_framework_tutorial'
--- simple-blog/backend/simpleblog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimpleblogConfig(AppConfig):
-    name = 'simpleblog'
